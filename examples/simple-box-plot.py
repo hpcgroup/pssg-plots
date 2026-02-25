6450871eef031d00584ef9e227538c0099f42044
Josh Davis <jhdavis@umd.edu>
2026-02-25 16:20:58 -0500

    Add example box plot script

---
+++ examples/simple-box-plot.py
@@ -0,0 +1,34 @@
+"""A simple box plot example"""
+
+import sys
+import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+sys.path.append(".")
+sys.path.append("..")
+from pssgplot import PlotEnvironment
+from pssgplot.boxplot import BoxPlot
+
+with PlotEnvironment(font_path=Path("../fonts/gillsans.ttf")):
+    df = pd.DataFrame(
+        {
+            "value": np.concatenate([np.random.normal(0, 1 + i, 100) for i in range(5)]),
+            "group": np.concatenate([[name] * 100 for name in ["A", "B", "C", "D", "E"]])
+
+        }
+    )
+
+    simple_box = BoxPlot()
+    simple_box.plot(
+        data=df, x="group", y="value", title="A simple box plot"
+    )
+
+    # Use .animate to save an animation of the plot
+    #simple_box.animate(by="column", save_dir=Path("output"))
+
+    # Use .show or .save to output the figure
+    os.makedirs("output", exist_ok=True)
+    simple_box.save("output/simple-box-plot.pdf", format="pdf")
